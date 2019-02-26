c1ed6166b8a34e6722c24244e9923265ffaf0b9c
cecespana <cedric.ortega06210@gmail.com>
2019-02-26 22:54:21 +0100

    Section 5 fini

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -32,15 +34,30 @@ class AdController extends AbstractController
      * @Route("/ads/create", name="ads_create")
      * @return Response
      */
-    public function create()
+    public function create(Request $request, ObjectManager $manager)
     {
         $newAd = new Ad();
 
         $form = $this->createForm(AnnonceType::class, $newAd);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($newAd);
+            $manager->flush();
+
+            $this->addFlash('success',
+            "Votre annonce <strong>{$newAd->getTitle() }</strong> a bien été enregistré !");
+
+            return $this->redirectToRoute('ads_show',['slug' => $newAd->getSlug()]
+            );
+        }
                                 
         return $this->render('ad/create.html.twig',[
             'form' => $form->createView()
         ]);
+
+        
 
     }
 
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -6,20 +6,40 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AnnonceType extends AbstractType
 {
+    
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        return ['label' => $label,
+                'attr' => ['placeholder' => $placeholder
+        ]
+        ];
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
-        ;
+            ->add('title', TextType::class, $this->getConfiguration("Titre","Saisir le titre de l'annonce"))
+            ->add('slug', TextType::class, $this->getConfiguration("Adresse web","Saisir l'adresse web (Automatique) "))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("Url de l'image principale","Saisir l'url de l'image principale'"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction","Saisir l'introduction de l'annonce"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description détaillée","Saisir la description de l'annonce"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres","Saisir le nombre de chambre de l'annonce"))
+            ->add('price', MoneyType::class, $this->getConfiguration("Prix de la nuit","Saisir le prix de la nuit"))
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
